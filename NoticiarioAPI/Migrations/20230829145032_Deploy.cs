ad8f1c206197069af884a3d89562cb2ffd79395e
João Victor Carneiro Aureliano <jo4ovictor10@hotmail.com>
2023-08-29 11:51:03 -0300

    deploy v2

---
+++ NoticiarioAPI/Migrations/20230829145032_Deploy.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoticiarioAPI.Migrations
+{
+    public partial class Deploy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Conteudo",
+                table: "Noticias",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Conteudo",
+                table: "Noticias",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
