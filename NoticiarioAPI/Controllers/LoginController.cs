63d4b2383d68670c0a1a12ca1b663f44dd55add1
João Victor Carneiro Aureliano <jo4ovictor10@hotmail.com>
2023-08-29 16:35:47 -0300

    Deploy k8s

--- NoticiarioAPI/Controllers/LoginController.cs
+++ NoticiarioAPI/Controllers/LoginController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using NoticiarioAPI.Context;
 using NoticiarioAPI.Domain.Models;
-using NoticiarioAPI.Exceptions;
 using NoticiarioAPI.Repository.Interfaces;
 
 namespace NoticiarioAPI.Controllers;
@@ -25,7 +22,6 @@ public class LoginController : ControllerBase
 
     [HttpPost]
     [Route("GeraToken")]
-    [AllowAnonymous]
     public ActionResult<TokenModel> AutenticaAsync(string email, string password)
     {
         var user = _uof.UsuarioRepository.AutenticaUser(email, password);
@@ -38,21 +34,6 @@ public class LoginController : ControllerBase
         else
         {
             return Unauthorized();
-        }
-
-    }
-
-    public ActionResult RunMigration()
-    {
-        try
-        {
-            _nContext.Database.Migrate();
-            return Ok();
-        }
-        catch (Exception ex)
-        {
-
-            throw new ErroNoBanco($"Houve um erro ao executar o Migrations {ex}");
         }
 
     }
