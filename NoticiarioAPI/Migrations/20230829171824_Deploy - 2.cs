e149c80fba0fa11b572614d37f563f53c1f83eed
João Victor Carneiro Aureliano <jo4ovictor10@hotmail.com>
2023-08-29 14:20:36 -0300

    v3

---
+++ NoticiarioAPI/Migrations/20230829171824_Deploy - 2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoticiarioAPI.Migrations
+{
+    public partial class Deploy2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
